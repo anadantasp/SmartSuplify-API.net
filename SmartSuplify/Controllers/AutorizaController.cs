5abeebd0d7289562bc39085fe6e1e8e459901465
anadantasp <caroldantasp@gmail.com>
2024-05-19 16:33:02 -0300

    ajustes finais integração API

--- SmartSuplify/Controllers/AutorizaController.cs
+++ SmartSuplify/Controllers/AutorizaController.cs
@@ -4,6 +4,7 @@ using Microsoft.IdentityModel.Tokens;
 using SmartSuplify.DTO;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace SmartSuplify.Controllers
@@ -26,7 +27,7 @@ namespace SmartSuplify.Controllers
 		}
 
 		[HttpPost("CadastrarUsuario")]
-		public async Task<ActionResult> RegisterUser([FromBody] UsuarioDTO usuarioDTO)
+		public async Task<ActionResult> RegisterUser([FromBody] UsuarioDTO usuarioDTO, HttpContext context)
 		{
 			var user = new IdentityUser
 			{
@@ -42,7 +43,10 @@ namespace SmartSuplify.Controllers
 				return BadRequest(result.Errors);
 			}
 			await _signInManager.SignInAsync(user, false);
-			return Ok(GeraToken(usuarioDTO));
+
+			var token = GeraToken(usuarioDTO, context);
+
+			return Ok(token);
 		}
 
 		[HttpPost("Login")]
@@ -61,7 +65,11 @@ namespace SmartSuplify.Controllers
 							);
 			if (resul.Succeeded)
 			{
-				return Ok(GeraToken(usuarioDTO));
+				var token = GeraToken(usuarioDTO, HttpContext);
+
+				
+
+				return Ok(token);
 			}
 			else
 			{
@@ -70,14 +78,15 @@ namespace SmartSuplify.Controllers
 			}
 		}
 
-		private UsuarioToken GeraToken(UsuarioDTO userInfo)
+		private UsuarioToken GeraToken(UsuarioDTO userInfo, HttpContext context)
 		{
+			var sessionId = GenerateSecureSessionId();
 			//Define declarações do usuário
 			var claims = new[]
 			{
 				new Claim(JwtRegisteredClaimNames.UniqueName, userInfo.Email),
 				new Claim("SmartSuplify", "UsuarioSmartSuplify"),
-				new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+				new Claim(JwtRegisteredClaimNames.Jti, sessionId)
 			};
 
 			//gera uma chave com base em um algoritmo simetrico
@@ -99,6 +108,22 @@ namespace SmartSuplify.Controllers
 			  expires: expiration,
 			  signingCredentials: credenciais);
 
+			var thirdClaim = claims[2];
+			var claimValue = thirdClaim.Value;
+
+			// Adiciona o ID da sessão no cabeçalho da resposta
+			Response.Headers.Add("JSESSIONID", sessionId);
+
+			// Adiciona o ID da sessão no cookie
+			var cookieOptions = new CookieOptions
+			{
+				HttpOnly = true,
+				Secure = context.Request.IsHttps,
+				SameSite = SameSiteMode.Lax
+			};
+			context.Response.Cookies.Append("JSESSIONID", sessionId, cookieOptions);
+
+
 			//retorna os dados com o token e informacoes
 			return new UsuarioToken()
 			{
@@ -106,7 +131,28 @@ namespace SmartSuplify.Controllers
 				Token = new JwtSecurityTokenHandler().WriteToken(token),
 				Expiration = expiration,
 				Message = "Token JWT OK"
+
 			};
+		}
+
+		// Método para gerar um ID de sessão seguro
+		private string GenerateSecureSessionId()
+		{
+			// Gera um array de bytes aleatórios
+			byte[] randomBytes = new byte[16]; // 128 bits
+			using (var rng = RandomNumberGenerator.Create())
+			{
+				rng.GetBytes(randomBytes);
+			}
+
+			// Converte os bytes para uma string hexadecimal
+			var sessionId = new StringBuilder();
+			foreach (var b in randomBytes)
+			{
+				sessionId.Append(b.ToString("x2"));
+			}
+
+			return sessionId.ToString();
 		}
 	}
 }
